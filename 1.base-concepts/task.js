2a6d5fcc561bd55e252fd0e3caccdbe454fbe495
Lostman87 <pin36@yandex.ru>
2022-09-20 20:28:10 +0300

    diskriminant

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,25 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
+   const arr = [];
+  const discriminant = b ** 2 - 4 * a * c;
+  let x1;
+  let x2;
+
+  if (discriminant == 0) {
+    x1 = -b / (2 * a)
+    arr[0] = x1;
+  }
+
+  if (discriminant > 0) {
+    x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+
+    arr[0] = x1;
+    arr[1] = x2;
+
+  }
   // код для задачи №1 писать здесь
+  
   return arr; // array
 }
 
