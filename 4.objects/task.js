950af6adabd5965bf6c2dd07cd0ce456046351d9
Lostman87 <pin36@yandex.ru>
2022-09-20 20:59:13 +0300

    objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,59 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
+let misha = new Student("Миша", "муж", 24)
+let petya = new Student("Петя", "муж", 32)  // Ваш код
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;//ваш код
 }
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
+    this.marks.push(mark);
+  } else {
+    this.marks.push(mark);
+  }
+};
 
+Student.prototype.addMarks = function (...args) {
+  if (this.marks === undefined) {
+    this.marks = [];
+    args.map(el => this.marks.push(el));
+  } else {
+    args.map(el => this.marks.push(el));
+  }
+};
+
+Student.prototype.getAverage = function () {
+  let sum = this.marks.reduce((sum, current) => sum + current)
+  this.mediumAriphetic = sum / this.marks.length // для округления в не рамок задания Number((sum / this.marks.length).toFixed(2))
+  return this.mediumAriphetic
+};
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+};
+
+//                                Проверка                   
+
+misha.addMark(5);
+misha.addMark(4);
+misha.addMarks(4, 4, 3, 5, 4, 3, 4);
+misha.getAverage();
+misha.exclude("балбес");
+
+petya.addMark(2);
+petya.addMark(3);
+petya.addMarks(2, 3, 2, 2, 2, 3, 2);
+petya.getAverage();
+petya.exclude("плохая успеваемость");
+
+console.log(misha);
+console.log(petya);
 // ваш код для остальных методов
