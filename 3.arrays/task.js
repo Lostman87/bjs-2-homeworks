3a3d4fb0b9b2c82d4b3aaa05f45decc45582320f
Lostman87 <pin36@yandex.ru>
2022-09-20 20:50:40 +0300

    arr

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,14 +1,12 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
+  let result = arr1.length === arr2.length && arr1.every((item, idx) => item === arr2[idx]);
   // Ваш код
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
+  let resultArr = arr.filter((positiveNumbers) => positiveNumbers > 0).filter((number) => number % 3 === 0).map((muliplicationTen) => muliplicationTen * 10);
   // Ваш код
 
   return resultArr; // array
